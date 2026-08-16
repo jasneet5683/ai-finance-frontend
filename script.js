334d7de6c5beff11f8811db772c3807973ba2257
jasneet5683 <jasneet.happy@gmail.com>
2026-08-16 22:57:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
  */
 
 //const BASE_URL = "https://api.finance.jasneet.uk";
-const BASE_URL_old = "https://ai-financial-production.up.railway.app";
+const BASE_URL = "https://ai-financial-production.up.railway.app";
 let currentStockData = null;
 let currentStockAnalysis = null;
 //let priceChartInstance = null; // Used to track and update the Chart.js instance
